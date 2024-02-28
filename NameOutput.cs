f621ef0faeb33d78444ed5e5e461e78d0ac1964f
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 10:12:37 +0500

    Name output

---
+++ NameOutput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameOutput
+{
+	internal class NameOutput
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите ваше имя: ");
+			string name = Console.ReadLine();
+
+			Console.Write("Введите символ для прямоугольника: ");
+			char symbol = Console.ReadKey(true).KeyChar;
+			Console.WriteLine();
+
+			string middleLine = symbol.ToString() + " " + name + " " + symbol.ToString();
+			string borderLine = String.Empty;
+			int widthRectangle = middleLine.Length;
+
+			for (int i = 0; i < widthRectangle; ++i)
+			{
+				borderLine += symbol.ToString();
+			}
+
+			Console.WriteLine(borderLine);
+			Console.WriteLine(middleLine);
+			Console.WriteLine(borderLine);
+		}
+	}
+}
